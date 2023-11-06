c658a1ff732b2f8b3e4ec6e0c4fcb7232d2aa288
Reyhan Cetin <90903952+rcteenx@users.noreply.github.com>
2023-11-06 18:27:33 +0300

    Create moveZeros.js
    CodeWars - 5 kyu

---
+++ moveZeros.js
@@ -0,0 +1,14 @@
+function moveZeros(arr) {
+  const a1 = [];
+  const a0 = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === 0) {
+      a0.push(arr[i]);
+    } else {
+      a1.push(arr[i]);
+    }
+  }
+  return a1.concat(a0);
+}
+
+// console.log(moveZeros([1, 2, 0, 1, 0, 1, 0, 3, 0, 1]));
